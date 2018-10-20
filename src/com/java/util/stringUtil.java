99dccf6ce8ed6ae359ba9bab85b28da76c7ba7a8
garin197 <953626691@qq.com>
2018-10-20 12:07:16 +0800

    写了几个类

--- src/com/java/util/stringUtil.java
+++ src/com/java/util/stringUtil.java
@@ -1,7 +1,21 @@
 package com.java.util;
-
+/**
+ * 字符串  工具类
+ * @author Garin
+ *
+ */
 public class stringUtil {
-	//代码还没写
-	//。。。。
-	//。。。。
+	/**
+	 * 检查字段是否为空
+	 * @param string
+	 * @return
+	 */
+	public static boolean isEmpty(String string) {
+		if (string==null||string.equals("")) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
 }
